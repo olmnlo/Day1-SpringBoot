2e0961263d349fc48c4e5e9893d0f115f6c804f1
olmnlo <hosamag54@gmail.com>
2025-07-15 11:49:24 +0300

    first commit

---
+++ src/main/java/org/example/day1introductiontospringboot/Day1IntroductionToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.day1introductiontospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1IntroductionToSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day1IntroductionToSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/day1introductiontospringboot/Day1IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day1introductiontospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1IntroductionToSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
